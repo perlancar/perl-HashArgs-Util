640d82d8eba0610c4dcb32223fe9ce7798e2abad
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-07-10 09:23:23 +0700

    Initial commit

---
+++ lib/HashArgs/Util.pm
@@ -0,0 +1,44 @@
+package HashArgs::Util;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw();
+
+sub select_args {
+}
+
+1;
+# ABSTRACT: Some routines to handle hash/keyword arguments
+
+=head1 SYNOPSIS
+
+ use HashArgs::Util qw(
+     select_args
+ );
+
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS
+
+None exported by default.
+
+=head2 select_args
+
+
+
+=head1 SEE ALSO
+
+Some other modules to handle hash/keyword arguments: L<Hash::AutoHash::Args>,
+L<Devel::TakeHashArgs>, L<Hash::Args>, L<Kwargs>.
+
+Some other hash utilities: L<Hash::MostUtils>
+
+=cut
